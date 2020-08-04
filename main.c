d4a7beb67999cf6066630a234df77a9291188e70
Tomasz Gardocki <tgardocki1@gmail.com>
2020-08-04 22:57:25 +0200

    Files passed as arguments

--- main.c
+++ main.c
@@ -3,24 +3,51 @@
 #include "scores.h"
 #include "database.h"
 
-int main() {
-	const char* filename = "scores.bin";
-	char header[SCORES_HEADER_LENGTH];
-	score_t score;
-	
-	databaseInit(32);
-	scoreAlloc(&score);
-	scoresOpen(filename);
-	memcpy(header, scoresGetHeader(), SCORES_HEADER_LENGTH);
-
-	while(scoresGetNext(&score))
-		databaseAdd(score);
-	
+void freeAllMemory(score_t* ptr) {
 	scoresClose();
-	scoreFree(&score);
-
-	databaseSave("scores_merged.bin", header);
+	scoreFree(ptr);
 	databaseFree();
+}
+
+int main(int argc, char* argv[]) {
+	char	header[SCORES_HEADER_LENGTH];
+	score_t	score;
+
+	if(argc > 1) {
+		databaseInit(32);
+		scoreAlloc(&score);
+
+		for(int i = 1; i < argc; i++) {
+			scoresOpen(argv[i]);
+
+			// header check
+			if(i > 1) {
+				if(strcmp(header, scoresGetHeader())) {
+					fprintf(stderr, "ERROR: Header mismatch! If you want to continue at your own risk, replace the header (first %d characters) in file %s.\n", SCORES_HEADER_LENGTH, argv[i]);
+					freeAllMemory(&score);
+					return 1;
+				}
+			} else
+				memcpy(header, scoresGetHeader(), SCORES_HEADER_LENGTH);
+
+			// writing
+			while(scoresGetNext(&score)) {
+				if(!databaseAdd(score)) {
+					fprintf(stderr, "ERROR: Out of memory!\n");
+					freeAllMemory(&score);
+					return 2;
+				}
+			}
+
+			scoresClose();
+		}
+
+		scoreFree(&score);
+		if(!databaseSave("scores_merged.bin", header))
+			fprintf(stderr, "ERROR: Could not write file!\n");
+		databaseFree();
+	} else
+		printf("Usage: %s [path to file 1] [path to file 2] ...\n", argv[0]);
 	return 0;
 }
-	
+
